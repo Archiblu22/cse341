4773e6ae73d5861a230691106f51eb7fee81f36a
archiblue22 <paguirre@byui.edu>
2021-09-16 14:41:05 -0600

    commit

--- section05/assign1.js
+++ section05/assign1.js
@@ -2,12 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-});
+// app.use('/', (req, res, next) => {
+//     console.log('First');
+//     next();
+// });
+
+// app.use('/users', (req, res, next) => {
+//     console.log('second');
+//     res.send('<h2>finished</h2>');
+// });
 
 app.use('/users', (req, res, next) => {
-    res.send('<h1>The "Users" Page</h1>');
-});
+    res.send('<h1>This is the "Users" page</h1>');
+})
+
+app.use('/' , (req, res, next) => {
+    console.log('Needs to go last');
+    res.send('<h1>Welcome to Express!</h1>');
+})
 
 app.listen(3000);
