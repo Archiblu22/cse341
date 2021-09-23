dc375a01873bbc7a5fbf2a5b74862df3eaf4c07c
archiblue22 <paguirre@byui.edu>
2021-09-23 17:25:13 -0600

    commit

--- section05/routes/admin.js
+++ section05/routes/admin.js
@@ -3,10 +3,8 @@ const path = require('path');
 const express = require('express');
 
 const productsController = require('../controllers/products');
-// const { title } = require('process');
 
 const router = express.Router();
-
 
 // /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
@@ -14,4 +12,4 @@ router.get('/add-product', productsController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
 
-module.routes = router;
+module.exports = router;
