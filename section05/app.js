e725cea24760e7f1ea6b787c2182b84823283847
archiblue22 <paguirre@byui.edu>
2021-09-30 07:06:59 -0600

    commit

--- section05/app.js
+++ section05/app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
+  // User.findById(1)
   //   .then(user => {
   //     req.user = user;
   //     next();
